c43226ee19178c49b1c8199d44ba010a8b42123d
Arupaka1152 <hkohama1152@gmail.com>
2022-11-06 11:44:16 +0900

    update login

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,76 +1,43 @@
 import React, { useState } from "react";
-import { Form, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./Login.css";
 
 function Login() {
 
-    const [ Name, setName] = useState("");
     const [ Email, setEmail] = useState("");
     const [ Password, setPassword] = useState("");
-    const [ PasswordForConfirmation, setPasswordForConfirmation] = useState("");
 
-    
+    const onSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        if (Email === "" || Password === "") return;
+        e.preventDefault();
+        console.log({ Email, Password });
+    }
+
     return (
-        <div>
-            <Card>
-                <Card.Body>
-                    <Form>
-                    <Form.Group id="email">
-                        <Form.Label>メールアドレス</Form.Label>
-                        <Form.Control 
-                            type={"email"}
-                            value={Email}
-                            onChange={(e) => setEmail(e.target.value)} 
-                            required />
-                    </Form.Group>
-                    <Form.Group id="password">
-                        <Form.Label>パスワード</Form.Label>
-                        <Form.Control 
-                            type={"password"}
-                            value={Password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required />
-                    </Form.Group>
-                    </Form>
-                </Card.Body>
-            </Card>
-
-
-
-            <Form>
-                    <Form.Group id="name" className="signup--input-box">
-                        <Form.Label>名前</Form.Label>
-                        <Form.Control 
-                            type={"name"}
-                            value={Name}
-                            onChange={(e) => setName(e.target.value)} 
-                            required />
-                    </Form.Group>
-                    <Form.Group id="email" className="signup--input-box">
-                        <Form.Label>メールアドレス</Form.Label>
-                        <Form.Control 
-                            type={"email"}
-                            value={Email}
-                            onChange={(e) => setEmail(e.target.value)} 
-                            required />
-                    </Form.Group>
-                    <Form.Group id="password" className="signup--input-box">
-                        <Form.Label>パスワード</Form.Label>
-                        <Form.Control 
-                            type={"password"}
-                            value={Password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required />
-                    </Form.Group>
-                    <Form.Group id="password-confirm" className="signup--input-box">
-                        <Form.Label>パスワード（確認用）</Form.Label>
-                        <Form.Control
-                            type={"password"}
-                            value={PasswordForConfirmation}
-                            onChange={(e) => setPasswordForConfirmation(e.target.value)}
-                            required />
-                      </Form.Group>
-                    </Form>
-        </div>
+        <form>
+            <div className="title">ログイン</div>
+            <div className="input-container ic2">
+                <input
+                    type="email"
+                    value={Email}
+                    placeholder="メールアドレス"
+                    onChange={(e) => setEmail(e.target.value)}
+                ></input>
+            </div>
+            <div className="input-container ic2">
+                <input
+                    type="password"
+                    value={Password}
+                    placeholder="パスワード"
+                    onChange={(e) => setPassword(e.target.value)}
+                ></input>
+            </div>
+            <button 
+                type="submit"
+                onClick={onSubmit}
+            >ログイン</button>
+            <Link to="/signup"><div className="signup" >アカウント登録はこちらから</div></Link>
+        </form>
     )
 }
 
