174eec66d8ca06a15149ec540c3c8958cdd5f7e0
Arupaka1152 <hkohama1152@gmail.com>
2022-11-06 11:37:16 +0900

    update signup

--- frontend/src/components/Signup.tsx
+++ frontend/src/components/Signup.tsx
@@ -1,34 +1,60 @@
 import React, { useState } from "react";
-import { Form, Button, Container, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "./Signup.css";
 
 function Signup() {
 
-    /* const [ Name, setName] = useState("");
+    const [ Name, setName] = useState("");
     const [ Email, setEmail] = useState("");
     const [ Password, setPassword] = useState("");
-    const [ PasswordForConfirmation, setPasswordForConfirmation] = useState(""); */
+    const [ PasswordForConfirmation, setPasswordForConfirmation] = useState("");
+
+    const onSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        if (Name === "" || Email === "" || Password === "" || PasswordForConfirmation === "") return;
+        e.preventDefault();
+        console.log({ Name, Email, Password, PasswordForConfirmation });
+    }
 
     return (
-        <form className="form">
+        <form>
             <div className="title">アカウント登録</div>
             <div className="input-container ic1">
-                <input className="input" type="text" />
-                <label className="placeholder">名前</label>
+                <input
+                    type="text"
+                    value={Name}
+                    placeholder="名前"
+                    onChange={(e) => setName(e.target.value)}
+                ></input>
             </div>
             <div className="input-container ic2">
-                <input className="input" type="text" />
-                <label className="placeholder">メールアドレス</label>
+                <input
+                    type="email"
+                    value={Email}
+                    placeholder="メールアドレス"
+                    onChange={(e) => setEmail(e.target.value)}
+                ></input>
             </div>
             <div className="input-container ic2">
-                <input className="input" type="text" />
-                <label className="placeholder">パスワード</label>
+                <input
+                    type="password"
+                    value={Password}
+                    placeholder="パスワード(半角英数字10文字以上)"
+                    onChange={(e) => setPassword(e.target.value)}
+                ></input>
             </div>
             <div className="input-container ic2">
-                <input className="input" type="text" />
-                <label className="placeholder">パスワード（確認用）</label>
+                <input
+                    type="password"
+                    value={PasswordForConfirmation}
+                    placeholder="パスワード(確認用)"
+                    onChange={(e) => setPasswordForConfirmation(e.target.value)}
+                ></input>
             </div>
-            <button type="submit" className="submit">登録</button>
+            <button 
+                type="submit"
+                onClick={onSubmit}
+            >登録</button>
+            <Link to="/login"><div className="login" >ログインはこちらから</div></Link>
         </form>
     )
 }
