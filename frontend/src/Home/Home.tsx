f6b232a27c150d29f07a94f82bbb4472c919356f
Arupaka1152 <hkohama1152@gmail.com>
2022-11-26 02:56:07 +0900

    create home

--- frontend/src/Home/Home.tsx
+++ frontend/src/Home/Home.tsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 function Home() {
     return (
         <div className="home-div">
+            <div className="home-title">Merci!</div>
+            <div className="home-subtitle">「ありがとう」を、贈ろう</div>
             <Link to="/signup"><div className="signup-link" >アカウント登録はこちらから</div></Link>
             <Link to="/login"><div className="login-link" >ログインはこちらから</div></Link>
         </div>
